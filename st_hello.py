33d7b4357f86853eee7340e49a5176abd1cf72d3
Geoff Matheson <geoffmatheson@gmail,com>
2024-07-25 13:29:33 +1000

    first commit

---
+++ st_hello.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title='Hello, Everyone!', page_icon=':smiley:')
+
+st.title('Hello, Everyone!')
+
+st.write('This is a simple Streamlit app.')
+
+st.sidebar.title("Select a page")
