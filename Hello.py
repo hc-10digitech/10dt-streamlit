42b578a0e67f00ed23cb30e98150760eb5ce8e4e
Geoff Matheson <111482451+mrmatho@users.noreply.github.com>
2024-08-18 10:24:44 +0000

    Adding comments to Hello

--- Hello.py
+++ Hello.py
@@ -9,21 +9,25 @@ st.set_page_config(page_title=TITLE, page_icon=':smiley',  layout="wide")
 st.title(TITLE)
 st.write('This is a simple Streamlit app.')
 
+# Heading is a little smaller
 st.header("This is a header. It's smaller than the other one")
 
+# Splitting the rest of the content into two columns
+# If the screen is too small, it will show one column
 column1, column2 = st.columns(2)
 
+# Uses a context manager to write to the columns
 with column1:
     st.write("This information is going on the left")
     if st.button("Press Me"):
         st.balloons()
         st.toast("Button pressed!", icon=":material/radio_button_checked:")
-    
+
+# Second column    
 with column2:
     st.write("This information is going on the right")
     st.image(image="./images/monster.png", caption="My Cute Monster")
     
-
 
 # This adds the nice message below the sidebar navigation
 st.sidebar.success("Select a page from the list above")
