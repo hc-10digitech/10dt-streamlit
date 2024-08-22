cbadffebaa6aa01b10cd05b6481f69025c70ff35
Geoff Matheson <111482451+mrmatho@users.noreply.github.com>
2024-08-22 00:31:20 +0000

    Added sample graphs. Might change later, not sure.

--- pages/2_📊_Graph.py
+++ pages/2_📊_Graph.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
+
 TITLE = "Graphing from a Spreadsheet"
 @st.cache_data
 def load_data():
@@ -11,7 +13,25 @@ st.title(TITLE)
 
 st.write('This is a simple Streamlit app that reads a spreadsheet and graphs the data. (or at least it will be)')
 st.write("At the moment all it does is show the data from the spreadsheet")
+# This gets called so that the data can be cached, rather than reloaded every time
 df = load_data()
 st.dataframe(df)
+
+
+# Create a bar chart based on this data
+#print(df.columns)
+st.bar_chart(df, x='Company',
+             y='Units sold (million)',
+             color='Console Name',
+             height=400)
+
+print(df['Released Year'].min(), df['Released Year'].max())
+df['Discontinuation Year'] = df['Discontinuation Year'].replace(0, 2024)
+df['Discontinuation Year'] = df['Discontinuation Year'].fillna(df['Discontinuation Year'].max())
+st.scatter_chart(df, 
+                 x='Released Year', 
+                 y='Discontinuation Year',
+                 color='Console Name', 
+                 size='Units sold (million)')
 
 st.sidebar.success("Select a page from the list above")
